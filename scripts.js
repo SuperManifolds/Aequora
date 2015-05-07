349f115e786787f99cf16306b6bfc5f4ba448638
xlexi <alex@delexious.com>
2015-05-07 15:18:01 +0200

    Obey our lord and saviour jslint.

--- scripts.js
+++ scripts.js
@@ -9,15 +9,14 @@ var previousNick = '', previousNickCount = 1, previousNickMessageId, previousNic
 Textual.nicknameSingleClicked = function (e) {
     "use strict";
     this.userNicknameSingleClickEvent(e);
-}
+};
 
 Textual.viewBodyDidLoad = function () {
     "use strict";
     Textual.fadeOutLoadingScreen(1.00, 0.95);
 
     window.twttr = (function (d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0],
-            t = window.twttr || {};
+        var js, fjs = d.getElementsByTagName(s)[0], t = window.twttr || {};
         if (d.getElementById(id)) {
             return t;
         }
@@ -38,17 +37,17 @@ Textual.viewBodyDidLoad = function () {
 
 function resizeImage(e) {
     "use strict";
-    setTimeout(function () {
+    function resizeAtInterval() {
         if (e.target.offsetHeight > (window.innerHeight - 150)) {
             e.target.style.height = (window.innerHeight - 150);
         }
-    }, 1000);
+    }
+    setTimeout(resizeAtInterval, 1000);
 }
 
 Textual.newMessagePostedToView = function (line) {
     "use strict";
-    var message, innerMessage, twitterRegex, x, link, links, twitterMatch, requestUrl, twitterRequest, getEmbeddedImages, i, len;
-    var selectNick, nickElement;
+    var message, innerMessage, twitterRegex, x, link, links, twitterMatch, requestUrl, twitterRequest, getEmbeddedImages, i, len, linksLen, selectNick, nickElement;
 
     function twitterWidgetLoaded() {
         var container = document.createElement("div");
@@ -60,31 +59,31 @@ Textual.newMessagePostedToView = function (line) {
 
     message = document.getElementById('line-' + line);
     innerMessage = message.querySelector(".innerMessage");
-    
+
     if (message.getAttribute("encrypted") === "true") {
         if (innerMessage.innerText.indexOf("+OK") !== -1) {
             message.setAttribute("encrypted", "failed");
         }
     }
-    
+
     /* The following is the intellectual property of April King, see LICENSE.md for more information */
-    
+
     // reset the message count and previous nick, when you rejoin a channel
     if (message.getAttribute('ltype') !== 'privmsg') {
         previousNick = '';
         previousNickCount = 1;
     }
-    
+
     if (message.getAttribute('ltype') === 'privmsg' || message.getAttribute('ltype') === 'action') {
         selectNick = message.querySelector(".sender");
-    
+
         // Delete the previous line's nick, if it was set to be deleted
         if (previousNickDelete === true) {
             nickElement = document.getElementById(previousNickMessageId).getElementsByClassName('sender')[0];
             nickElement.classList.add("f");
             nickElement.style.color = window.getComputedStyle(nickElement).backgroundColor;
         }
-    
+
         // Track the nicks that submit messages, so that we can space out everything
         if ((previousNick === selectNick.innerHTML) && (previousNickCount < 10) && (message.getAttribute('ltype') !== 'action')) {
             previousNickDelete = true;
@@ -94,11 +93,11 @@ Textual.newMessagePostedToView = function (line) {
             previousNickCount = 1;
             previousNickDelete = false;
         }
-    
+
         // Track the previous message's id
         previousNickMessageId = message.getAttribute('id');
     }
-    
+
     /* --------------------------------------------------------------------------------------------------------------------------------------*/
 
     twitterRegex = /http(s)?:\/\/(www\.)?twitter\.com\/[A-Za-z0-9_\-]*\/status\/([0-9]*)/;
@@ -106,7 +105,8 @@ Textual.newMessagePostedToView = function (line) {
     if (innerMessage) {
         links = innerMessage.querySelectorAll("a");
         if (links) {
-            for (x = 0; x < links.length; x += 1) {
+            linksLen = links.length;
+            for (x = 0; x < linksLen; x += 1) {
                 link = links[x].getAttribute("href");
 
                 twitterMatch = twitterRegex.exec(link);
@@ -123,70 +123,76 @@ Textual.newMessagePostedToView = function (line) {
 
     getEmbeddedImages = message.querySelectorAll("img");
     if (getEmbeddedImages) {
-        for (i = 0, len = getEmbeddedImages.length; i < len; i += 1) {
+        len = getEmbeddedImages.length;
+        for (i = 0; i < len; i += 1) {
             getEmbeddedImages[i].onload = resizeImage;
         }
     }
-    window.updateNicknameAssociatedWithNewMessage(message);
+    this.updateNicknameAssociatedWithNewMessage(message);
 };
-
-function updateNicknameAssociatedWithNewMessage(e) {
-    "use strict";
-	/* We only want to target plain text messages. */
-	var elementType = e.getAttribute("ltype");
-    var acceptedElementTypes = ["privmsg", "action", "notice"];
-	if (acceptedElementTypes.indexOf(elementType) !== -1) {
-		/* Get the nickname information. */
-		var senderSelector = e.querySelector(".sender");
-		if (senderSelector) {
-			/* Is this a mapped user? */
-			var nickname = senderSelector.getAttribute("nickname");
-
-			/* If mapped, toggle status on for new message. */
-			if (mappedSelectedUsers.indexOf(nickname) > -1) {
-				toggleSelectionStatusForNicknameInsideElement(senderSelector);
-			}
-		}
-	}
-}
 
 function toggleSelectionStatusForNicknameInsideElement(e) {
     "use strict";
-	/* e is nested as the .sender so we have to go three parents
-	 up in order to reach the parent div that owns it. */
-	var parentSelector = e.parentNode;
-    for (var i = 0; i < 3; i += 1) {
-        parentSelector = parentSelector.parentNode;
-     }
+    var i, parentSelector = e.parentNode;
 
-	parentSelector.classList.toggle("selectedUser");
+    /* e is nested as the .sender so we have to go three parents
+    up in order to reach the parent div that owns it. */
+    for (i = 0; i < 3; i += 1) {
+        parentSelector = parentSelector.parentNode;
+    }
+
+    parentSelector.classList.toggle("selectedUser");
+}
+
+function updateNicknameAssociatedWithNewMessage(e) {
+    "use strict";
+    var elementType, acceptedElementTypes, senderSelector, nickname;
+    /* We only want to target plain text messages. */
+    elementType = e.getAttribute("ltype");
+    acceptedElementTypes = ["privmsg", "action", "notice"];
+    if (acceptedElementTypes.indexOf(elementType) !== -1) {
+        /* Get the nickname information. */
+        senderSelector = e.querySelector(".sender");
+        if (senderSelector) {
+            /* Is this a mapped user? */
+            nickname = senderSelector.getAttribute("nickname");
+
+            /* If mapped, toggle status on for new message. */
+            if (mappedSelectedUsers.indexOf(nickname) > -1) {
+                toggleSelectionStatusForNicknameInsideElement(senderSelector);
+            }
+        }
+    }
 }
 
 function userNicknameSingleClickEvent(e) {
     "use strict";
-	/* This is called when the .sender is clicked. */
-	var nickname = e.getAttribute("nickname");
-	/* Toggle mapped status for nickname. */
-	var mappedIndex = mappedSelectedUsers.indexOf(nickname);
+    var nickname, mappedIndex, i, len, documentBody, allLines, sender;
 
-	if (mappedIndex == -1) {
-		mappedSelectedUsers.push(nickname);
-	} else {
-		mappedSelectedUsers.splice(mappedIndex, 1);
-	}
+    /* This is called when the .sender is clicked. */
+    nickname = e.getAttribute("nickname");
+    /* Toggle mapped status for nickname. */
+    mappedIndex = mappedSelectedUsers.indexOf(nickname);
 
-	/* Gather basic information. */
-    var documentBody = document.getElementById("body_home");
+    if (mappedIndex === -1) {
+        mappedSelectedUsers.push(nickname);
+    } else {
+        mappedSelectedUsers.splice(mappedIndex, 1);
+    }
 
-    var allLines = documentBody.querySelectorAll('div[ltype="privmsg"], div[ltype="action"]');
+    /* Gather basic information. */
+    documentBody = document.getElementById("body_home");
 
-	/* Update all elements of the DOM matching conditions. */
-    for (var i = 0, len = allLines.length; i < len; i++) {
-        var sender = allLines[i].querySelectorAll(".sender");
+    allLines = documentBody.querySelectorAll('div[ltype="privmsg"], div[ltype="action"]');
+
+    /* Update all elements of the DOM matching conditions. */
+    len = allLines.length;
+    for (i = 0; i < len; i += 1) {
+        sender = allLines[i].querySelectorAll(".sender");
 
         if (sender.length > 0) {
             if (sender[0].getAttribute("nickname") === nickname) {
-				toggleSelectionStatusForNicknameInsideElement(sender[0]);
+                toggleSelectionStatusForNicknameInsideElement(sender[0]);
             }
         }
     }
